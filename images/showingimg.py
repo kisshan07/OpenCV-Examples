96a4aa8e9ec4b2b38d2a2706115667d6b69e491b
kisshan07 <94780838+kisshan07@users.noreply.github.com>
2021-12-30 16:30:52 +0530

    Add files via upload

---
+++ images/showingimg.py
@@ -0,0 +1,22 @@
+"""
+THIS CODE IS HELP YOU TO UNDERSTAND SOME OF THE OPENCV TOPICS .. THANK U NOW ENJOY..
+"""
+
+import cv2 as cv
+
+#importing libraries
+
+img = cv.imread(filename="")
+"""
+TIP:- You can check about cv.imread() fuction in readingimg folder . Thank You.
+"""
+
+cv.imshow(winname="", img)
+
+"""
+cv.imshow() function takes two parameter and bot are important.
+* first one is winnname which is the window title in which your image going to show up.
+* second one is the img which you want to display
+"""
+
+cv.waitKey(0) #this is very important if this command is not passed the image is appear for just fraction of second.
